cfae90d9c91a13a960672ceecb4547ce815872be
Gurvan Le Direach <gurvan.le-direach@etu.u-bordeaux.fr>
2021-05-22 23:33:52 +0200

    conversion library to package

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='rostron_utils',
-    author='NAELIC',
-    description='library for rostron_navigation',
-    packages=['utils'],
-    long_descriptio=open('README.md').read()
-)
