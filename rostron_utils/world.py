5b00cf2ef6b2b17559d4a98d57afdd7d6f09aa94
Etienne Schmitz <contact@etienne-schmitz.com>
2021-05-30 20:01:12 +0200

    update

--- rostron_utils/world.py
+++ rostron_utils/world.py
@@ -5,32 +5,29 @@ from rclpy.node import Node
 # from rclpy.subscription import Subscription
 from .decorators import singleton
 
+
 @singleton
 class World():
     node_: Node = None
 
     receive_once = [False]
 
-    field : Optional[Field] = None
-    field_changed : bool = False
+    field: Optional[Field] = None
+    field_changed: bool = False
 
     def init(self, node: Node) -> None:
         self.node_ = node
+        node.create_subscription(Field, 'field', self.field_callback, 10)
 
-        node.create_subscription(Field, 'field', self.field_callback)
+    def ready(self) -> None:
+        while not(all(self.receive_once)):
+            self.node_.get_logger().info('Waiting to receive all topics once...')
+        self.node_.get_logger().info('Waiting finished')
 
-    
     def field_callback(self, msg: Field) -> None:
+        self.node_.get_logger().info('receive')
+
         if self.field != msg:
             self.receive_once[0] = True
             self.field = msg
             self.field_changed = True    
-
-class WorldWaitable(Waitable):
-    def is_ready(self, _):
-        ready = True
-
-        for receive in World().receive_once:
-            if not(receive):
-                ready = False
-        return ready
